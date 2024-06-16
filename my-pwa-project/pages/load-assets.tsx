087810673a0cb80b27066815dd931c6e6286d420
Mario Micklisch <mario@favo.org>
2024-06-16 16:05:09 +0200

    Fix typo

--- my-pwa-project/pages/load-assets.tsx
+++ my-pwa-project/pages/load-assets.tsx
@@ -15,7 +15,7 @@ const LoadAssets = () => {
 					You can visit https://faucet.vecha.in and claim free VET or VTHO for your developer wallet.
 				</p>
 				<p className='mt-2 text-sm text-gray-300'>
-					Your Account Abstraction Wallet is a smart contract that can be instructed to interact on the user's behalf. On the very first action, the contract is deployed.
+					Your Account Abstraction Wallet is a smart contract that can be instructed to interact on the users behalf. On the very first action, the contract is deployed.
 				</p>
 				<p className='mt-2 text-sm text-gray-300'>
 					Only Privy has ownership and full access to the Account Abstraction Wallet.
